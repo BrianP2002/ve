aae6c8301b665b54d05dd6f5abe01898bbd7c49d
Lin Ha <halin2002@163.com>
2025-09-23 23:32:00 -0500

    updated .gitignore

--- CMakeFiles/distrib.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/distrib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/distrib.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
