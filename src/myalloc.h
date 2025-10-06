9e3333feb2f9caf91ac6461a63f9e81db0a2b8d9
Lin Ha <halin2002@163.com>
2025-10-05 21:00:57 -0500

    trying to resolve the GMP not found problem

--- src/myalloc.h
+++ src/myalloc.h
@@ -4,6 +4,7 @@
 #include "global.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 extern pointer myallocf PT((size_t, bool, char *, int));
 extern void myfreef PT((pointer, char *, int));
